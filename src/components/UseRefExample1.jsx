6c26c8badb30cd2261ec175a8740a88ca23957bb
Erick <12631133+erick207@users.noreply.github.com>
2024-04-11 14:10:38 -0300

    add comments

--- src/components/UseRefExample1.jsx
+++ src/components/UseRefExample1.jsx
@@ -2,11 +2,13 @@ import { useRef } from 'react'
 
 function UseRefExample1() {
   const inputRef = useRef()
-  const paraRef = useRef()
+  const paraRef = useRef() // reference to p el
 
   const onSubmit = (e) => {
     e.preventDefault()
     console.log(inputRef.current.value)
+    // don't give useRef this usage just for the hell of it though there may be some cases where its needed
+    // you can use for modifying its own value
     inputRef.current.value = 'Hello'
     inputRef.current.style.backgroundColor = 'peachpuff'
     paraRef.current.innerText = 'Goodbye'
